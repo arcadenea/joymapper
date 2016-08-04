0542d390d0fb737fd36e0fe7f56ab098021306b5
Jorge Luis Cabral <baigosoft@gmail.com>
2016-08-03 22:57:49 -0300

    agregado configuracion en archivo de texto

--- uinput.c
+++ uinput.c
@@ -28,39 +28,178 @@ int fd[2];
 
 int buttonmap[2][15];
 
-void initButtonMap(){
+static int readbutton(char *string_button);
 
-buttonmap[0][0]=KEY_UP;//up
-buttonmap[0][1]=KEY_DOWN;//down
-buttonmap[0][2]=KEY_LEFT;//left
-buttonmap[0][3]=KEY_RIGHT;//right
-buttonmap[0][4]=KEY_LEFTCTRL;//button1
-buttonmap[0][5]=KEY_LEFTALT;//button2
-buttonmap[0][6]=KEY_SPACE;//button3
-buttonmap[0][7]=KEY_LEFTSHIFT;//button4
-buttonmap[0][8]=KEY_Z;//button5
-buttonmap[0][9]=KEY_X;//button6
-buttonmap[0][10]=KEY_C;//button7
-buttonmap[0][11]=KEY_1;//start
-buttonmap[0][12]=KEY_5;//select
-buttonmap[0][13]=KEY_V;//button10
-buttonmap[0][14]=KEY_B;//button11
+static int readbutton(char *string_button)
+{
 
-buttonmap[1][0]=KEY_R;//up
-buttonmap[1][1]=KEY_F;//down
-buttonmap[1][2]=KEY_D;//left
-buttonmap[1][3]=KEY_G;//right
-buttonmap[1][4]=KEY_A;//button1
-buttonmap[1][5]=KEY_S;//button2
-buttonmap[1][6]=KEY_Q;//button3
-buttonmap[1][7]=KEY_W;//button4
-buttonmap[1][8]=KEY_E;//button5
-buttonmap[1][9]=KEY_D;//button6
-buttonmap[1][10]=KEY_O;//button7
-buttonmap[1][11]=KEY_L;//start
-buttonmap[1][12]=KEY_P;//select
-buttonmap[1][13]=KEY_6;//button10
-buttonmap[1][14]=KEY_2;//button11
+	if(strcmp(string_button,"KEY_UP") == 0)return KEY_UP;
+	if(strcmp(string_button,"KEY_DOWN") == 0)return KEY_DOWN;
+	if(strcmp(string_button,"KEY_LEFT") == 0)return KEY_LEFT;
+	if(strcmp(string_button,"KEY_RIGHT") == 0)return KEY_RIGHT;
+	if(strcmp(string_button,"KEY_A") == 0)return KEY_A;
+	if(strcmp(string_button,"KEY_B") == 0)return KEY_B;
+	if(strcmp(string_button,"KEY_C") == 0)return KEY_C;
+	if(strcmp(string_button,"KEY_D") == 0)return KEY_D;
+	if(strcmp(string_button,"KEY_E") == 0)return KEY_E;
+	if(strcmp(string_button,"KEY_F") == 0)return KEY_F;
+	if(strcmp(string_button,"KEY_G") == 0)return KEY_G;
+	if(strcmp(string_button,"KEY_H") == 0)return KEY_H;
+	if(strcmp(string_button,"KEY_I") == 0)return KEY_I;
+	if(strcmp(string_button,"KEY_J") == 0)return KEY_J;
+	if(strcmp(string_button,"KEY_K") == 0)return KEY_K;
+	if(strcmp(string_button,"KEY_L") == 0)return KEY_L;
+	if(strcmp(string_button,"KEY_M") == 0)return KEY_M;
+	if(strcmp(string_button,"KEY_N") == 0)return KEY_N;
+	if(strcmp(string_button,"KEY_O") == 0)return KEY_O;
+	if(strcmp(string_button,"KEY_P") == 0)return KEY_P;
+	if(strcmp(string_button,"KEY_Q") == 0)return KEY_Q;
+	if(strcmp(string_button,"KEY_R") == 0)return KEY_R;
+	if(strcmp(string_button,"KEY_S") == 0)return KEY_S;
+	if(strcmp(string_button,"KEY_T") == 0)return KEY_T;
+	if(strcmp(string_button,"KEY_U") == 0)return KEY_U;
+	if(strcmp(string_button,"KEY_V") == 0)return KEY_V;
+	if(strcmp(string_button,"KEY_W") == 0)return KEY_W;
+	if(strcmp(string_button,"KEY_X") == 0)return KEY_X;
+	if(strcmp(string_button,"KEY_Y") == 0)return KEY_Y;
+	if(strcmp(string_button,"KEY_Z") == 0)return KEY_Z;
+	if(strcmp(string_button,"KEY_0") == 0)return KEY_0;
+	if(strcmp(string_button,"KEY_1") == 0)return KEY_1;
+	if(strcmp(string_button,"KEY_2") == 0)return KEY_2;
+	if(strcmp(string_button,"KEY_3") == 0)return KEY_3;
+	if(strcmp(string_button,"KEY_4") == 0)return KEY_4;
+	if(strcmp(string_button,"KEY_5") == 0)return KEY_5;
+	if(strcmp(string_button,"KEY_6") == 0)return KEY_6;
+	if(strcmp(string_button,"KEY_7") == 0)return KEY_7;
+	if(strcmp(string_button,"KEY_8") == 0)return KEY_8;
+	if(strcmp(string_button,"KEY_9") == 0)return KEY_9;
+	if(strcmp(string_button,"KEY_SPACE") == 0)return KEY_SPACE;
+	if(strcmp(string_button,"KEY_LEFTCTRL") == 0)return KEY_LEFTCTRL;
+	if(strcmp(string_button,"KEY_RIGHTCTRL") == 0)return KEY_RIGHTCTRL;
+	if(strcmp(string_button,"KEY_LEFTALT") == 0)return KEY_LEFTALT;
+	if(strcmp(string_button,"KEY_RIGHTALT") == 0)return KEY_RIGHTALT;
+	if(strcmp(string_button,"KEY_LEFTSHIFT") == 0)return KEY_LEFTSHIFT;
+	if(strcmp(string_button,"KEY_RIGHTSHIFT") == 0)return KEY_RIGHTSHIFT;
+	if(strcmp(string_button,"KEY_ESC") == 0)return KEY_ESC;
+	if(strcmp(string_button,"KEY_F1") == 0)return KEY_F1;
+	if(strcmp(string_button,"KEY_F2") == 0)return KEY_F2;
+	if(strcmp(string_button,"KEY_F3") == 0)return KEY_F3;
+	if(strcmp(string_button,"KEY_F4") == 0)return KEY_F4;
+	if(strcmp(string_button,"KEY_F5") == 0)return KEY_F5;
+	if(strcmp(string_button,"KEY_F6") == 0)return KEY_F6;
+	if(strcmp(string_button,"KEY_F7") == 0)return KEY_F7;
+	if(strcmp(string_button,"KEY_F8") == 0)return KEY_F8;
+	if(strcmp(string_button,"KEY_F9") == 0)return KEY_F9;
+	if(strcmp(string_button,"KEY_F10") == 0)return KEY_F10;
+	if(strcmp(string_button,"KEY_F11") == 0)return KEY_F11;
+	if(strcmp(string_button,"KEY_F12") == 0)return KEY_F12;
+	if(strcmp(string_button,"KEY_ENTER") == 0)return KEY_ENTER;
+	if(strcmp(string_button,"KEY_TAB") == 0)return KEY_TAB;
+	if(strcmp(string_button,"KEY_BACKSPACE") == 0)return KEY_BACKSPACE;
+	if(strcmp(string_button,"KEY_PAGEUP") == 0)return KEY_PAGEUP;
+	if(strcmp(string_button,"KEY_PAGEDOWN") == 0)return KEY_PAGEDOWN;
+	if(strcmp(string_button,"KEY_INSERT") == 0)return KEY_INSERT;
+	if(strcmp(string_button,"KEY_DELETE") == 0)return KEY_DELETE;
+	if(strcmp(string_button,"KEY_COMMA") == 0)return KEY_COMMA;
+	if(strcmp(string_button,"KEY_DOT") == 0)return KEY_DOT;
+	if(strcmp(string_button,"KEY_SLASH") == 0)return KEY_SLASH;
+
+	return KEY_UP;
+
+
+}
+
+void initButtonMap()
+{
+
+	int i = 0;
+	int j = 0;
+	
+	FILE *fconfig;
+
+	fconfig = fopen("config.txt","r");
+
+	char fline[300];//line of the file readed
+	
+	char njoybutton[20];//joy button to compare
+
+	char nbutton[20];//name of the button readed
+
+	char sbutton[20];//button readed
+
+	int button = 0;//joy button integer
+
+	int chot = 100;
+
+	while(!feof(fconfig))
+	{
+	
+		fscanf(fconfig,"%s %s",nbutton,sbutton);
+
+		printf("%s %d\n",nbutton,strlen(nbutton));
+		printf("%s\n",sbutton);
+
+		sprintf(njoybutton,"JOY%d_BUTTON_%d",i,j);
+
+		printf("%s %d\n",njoybutton,strlen(njoybutton));		
+
+		if(strcmp(nbutton,njoybutton)==0)
+		{
+			button = readbutton(sbutton);
+			printf("%d\n",button);
+			printf("\n");
+		}//if		
+				
+		buttonmap[i][j] = button;
+
+		memset(nbutton,0,20);
+		memset(sbutton,0,20);
+		memset(njoybutton,0,20);
+
+		j++;
+
+		if(j > 14)
+		{
+			j=0;
+			i++;
+			if(i > 1)break;
+		}//if
+
+		memset(fline,0,300);
+
+	}//while
+	
+		/*buttonmap[0][0]=KEY_UP;//up
+		buttonmap[0][1]=KEY_DOWN;//down
+		buttonmap[0][2]=KEY_LEFT;//left
+		buttonmap[0][3]=KEY_RIGHT;//right
+		buttonmap[0][4]=KEY_LEFTCTRL;//button1
+		buttonmap[0][5]=KEY_LEFTALT;//button2
+		buttonmap[0][6]=KEY_SPACE;//button3
+		buttonmap[0][7]=KEY_LEFTSHIFT;//button4
+		buttonmap[0][8]=KEY_Z;//button5
+		buttonmap[0][9]=KEY_X;//button6
+		buttonmap[0][10]=KEY_C;//button7
+		buttonmap[0][11]=KEY_1;//start
+		buttonmap[0][12]=KEY_5;//select
+		buttonmap[0][13]=KEY_V;//button10
+		buttonmap[0][14]=KEY_B;//button11
+
+		buttonmap[1][0]=KEY_R;//up
+		buttonmap[1][1]=KEY_F;//down
+		buttonmap[1][2]=KEY_D;//left
+		buttonmap[1][3]=KEY_G;//right
+		buttonmap[1][4]=KEY_A;//button1
+		buttonmap[1][5]=KEY_S;//button2
+		buttonmap[1][6]=KEY_Q;//button3
+		buttonmap[1][7]=KEY_W;//button4
+		buttonmap[1][8]=KEY_E;//button5
+		buttonmap[1][9]=KEY_D;//button6
+		buttonmap[1][10]=KEY_O;//button7
+		buttonmap[1][11]=KEY_L;//start
+		buttonmap[1][12]=KEY_P;//select
+		buttonmap[1][13]=KEY_6;//button10
+		buttonmap[1][14]=KEY_2;//button11*/
 
 }
 
